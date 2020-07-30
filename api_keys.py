5c29ed98d6b5bb713692720992101e5ddbc91279
luiscavazos <luiscavazos@github.com>
2020-07-30 17:12:11 -0500

    V0.5

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
-
+weather_api_key = "a34154200c5d4c099a9e9ed04a7d4e1e"
+weather_api_key_2 = "a34154200c5d4c099a9e9ed04a7d4e1e"
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBEsOrhKjPG0JjTUHHhbbtWFjCNS_UQ6Nw"
